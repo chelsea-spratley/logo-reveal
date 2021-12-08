f08ecfefb3abc06ef4437fc50b1c5adb06c8d04c
chelsea-spratley <cs3624@drexel.edu>
2021-12-07 20:56:53 -0500

    new

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -16,21 +16,66 @@ const mainTL = gsap.timeline();
 function logo(){
     const tl = gsap.timeline();
     tl.from("#greenlogo",{alpha:"0",y:"+=300", duration:2, scaleY:2});
+
     return tl;
 }
 
 function words(){
     const tl = gsap.timeline();
    tl.from("#Pennsylvania",{alpha:0 })
+
+   return tl;
 }
 
 function dirt(){
     const tl = gsap.timeline();
-    tl.from("#d1", {alpha:0,rotate:120, duration:0.1})
+    tl.from("#d1", {alpha:0,rotate:120, duration:0.1,opacity:0})
     .from("#d2", {alpha:0,rotate:-120, duration:0.1})
     .from("#d3", {alpha:0,rotate:120, duration:0.1})
-
+    .from("#d4", {alpha:0,rotate:120, duration:0.1})
+    .from("#d5", {alpha:0,rotate:120, duration:0.1})
+    .from("#d6", {alpha:0,rotate:120, duration:0.1})
+    .from("#d7", {alpha:0,rotate:120, duration:0.1})
+    .from("#d8", {alpha:0,rotate:120, duration:0.1})
+    .from("#d9", {alpha:0,rotate:120, duration:0.1})
+    .from("#d10", {alpha:0,rotate:120, duration:0.1})
+    .from("#d11", {alpha:0,rotate:120, duration:0.1})
+    .from("#d12", {alpha:0,rotate:120, duration:0.1})
+    .from("#d13", {alpha:0,rotate:120, duration:0.1})
+    .from("#d14", {alpha:0,rotate:120, duration:0.1})
+    .from("#d15", {alpha:0,rotate:120, duration:0.1})
+    .from("#d16", {alpha:0,rotate:120, duration:0.1})
+    .from("#d17", {alpha:0,rotate:120, duration:0.1})
+    .from("#d18", {alpha:0,rotate:120, duration:0.1})
+    .from("#d19", {alpha:0,rotate:120, duration:0.1})
+    .from("#d20", {alpha:0,rotate:120, duration:0.1})
+    .from("#d21", {alpha:0,rotate:120, duration:0.1})
+    .from("#d22", {alpha:0,rotate:120, duration:0.1})
+    .from("#d23", {alpha:0,rotate:120, duration:0.1})
+    .from("#d24", {alpha:0,rotate:120, duration:0.1})
+    .from("#d25", {alpha:0,rotate:120, duration:0.1})
+    .from("#d26", {alpha:0,rotate:120, duration:0.1})
+    .from("#d27", {alpha:0,rotate:120, duration:0.1})
+    .from("#d28", {alpha:0,rotate:120, duration:0.1})
+    .from("#d29", {alpha:0,rotate:120, duration:0.1})
+    .from("#d30", {alpha:0,rotate:120, duration:0.1})
+    .from("#d31", {alpha:0,rotate:120, duration:0.1})
+    .from("#d32", {alpha:0,rotate:120, duration:0.1})
+    .from("#d33", {alpha:0,rotate:120, duration:0.1})
+    .from("#d34", {alpha:0,rotate:120, duration:0.1})
+    .from("#d35", {alpha:0,rotate:120, duration:0.1})
+    .from("#d36", {alpha:0,rotate:120, duration:0.1})
+    .from("#d37", {alpha:0,rotate:120, duration:0.1})
+    .from("#d38", {alpha:0,rotate:120, duration:0.1})
+    .from("#d39", {alpha:0,rotate:120, duration:0.1})
+    .from("#d40", {alpha:0,rotate:120, duration:0.1})
+    .from("#d41", {alpha:0,rotate:120, duration:0.1})
+    .from("#d42", {alpha:0,rotate:120, duration:0.1})
+    .from("#d43", {alpha:0,rotate:120, duration:0.1})
+    .from("#d44", {alpha:0,rotate:120, duration:0.1})
+    .from("#d45", {alpha:0,rotate:120, duration:0.1})
     
+    return tl;
 }
 
 
